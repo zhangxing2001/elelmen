f299c187688a6d472397e0f1e47cdf1776376b35
zhangxing1 <15290324477@163.com>
2021-11-15 16:34:49 +0800

    docs(初始化项目环境): 增加 esLint prettier git cz 约束

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
@@ -11,8 +11,14 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
+  // data() {
+  //   return {
+  //     arr: [1, 2, 3]
+  //   }
+  // },
   components: {
     HelloWorld
-  }
+  },
+  created() {}
 }
 </script>
