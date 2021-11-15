a40f33db6135a5b670795b193119227ead2c3947
zhangxing1 <15290324477@163.com>
2021-11-15 16:47:05 +0800

    refactor(初始化项目结构): 初始化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,4 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <h3>这是首页</h3>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  // data() {
-  //   return {
-  //     arr: [1, 2, 3]
-  //   }
-  // },
-  components: {
-    HelloWorld
-  },
-  created() {}
-}
-</script>
+<script setup></script>
